7c458b5442d269b4a9622dafb4caabecad186d7f
Ewelina Chmielewska <ewelkos.chmielewska@gmail.com>
2018-12-30 23:42:38 +0100

    Corrected Linear Algorithm (MyAlgorithm)
    MyAlgorithm returns minimal rectangle with minimal number of reflected points. Code looks terrible so it has to be rewritten.

--- MyAlgorithm/MyAlgorithm.hpp
+++ MyAlgorithm/MyAlgorithm.hpp
@@ -15,13 +15,14 @@ class MyAlgorithm
     int countCosts(int, int, int, int, int);
     void dividePointsIntoUpperAndBottomGroup(std::vector<Point2D>, std::vector<Point2D>&, std::vector<Point2D>&);
     std::vector<Point2D> findMinRectangle(std::vector<Point2D>&, int, int, int, int);
-    bool isInArea(Point2D, std::vector<Point2D>);
+   // bool isInArea(Point2D, std::vector<Point2D>);
     std::vector<Point2D> mergeVectors(std::vector<Point2D>&, std::vector<Point2D>&, std::vector<Point2D>&, std::vector<Point2D>&);
     std::vector<Point2D> separateIntersectionPointsFromRectangles(std::vector<Point2D>&, std::vector<Point2D>&);
     void showVector(std::vector<Point2D>);
     std::vector<Point2D> sumVectors (std::vector<Point2D>&, std::vector<Point2D>&);
     void reflectRectangle(std::vector<Point2D>&, std::vector<Point2D>);
-
+    void checkCases(std::vector<Point2D> &result, std::vector<Point2D> &recVec, int cost1, int cost2, int cost3, int cost4, std::vector<Point2D> points);
+    std::vector<Point2D> findMinRectangle(std::vector<Point2D> &rectanglesSurroundingConvexHulls, int cost1, int cost2, int cost3, int cost4, std::vector<Point2D> points);
 
     private:
 };
